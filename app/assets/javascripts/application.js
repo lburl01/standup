28ed16baa0eed79569adaa9c42d3673ee91e98f0
Samantha Davis <samantha.shea.davis@gmail.com>
2016-11-02 16:27:31 -0400

    added angular controller, services and factories folders, renamed pages.js application.js, error can't find angular.min.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+//= require angular/angular.min.js
+//= require_tree ./services
+//= require_tree ./factories
+//= require_tree ./controllers

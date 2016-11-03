21609f388f477ca7cb9b4ecbb3f2234758712430
Lori Burleson <lori.burleson@gmail.com>
2016-11-03 09:47:22 -0400

    Added comma to redirec uri

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
-    redirect_uri:"https://virtual-standup.herokuapp.com/auth/google_oauth2/callback"
+    redirect_uri:"https://virtual-standup.herokuapp.com/auth/google_oauth2/callback",
     scope: "email, profile, calendar"
   }
 end

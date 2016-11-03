6ff7f304b81601a4b231e323cdceac07ae36454b
Lori Burleson <lori.burleson@gmail.com>
2016-11-03 17:03:59 -0400

    Printing all necessary data as json for timeline page

--- app/controllers/bulbs_controller.rb
+++ app/controllers/bulbs_controller.rb
@@ -1,20 +1,23 @@
 class BulbsController < ApplicationController
 
   def index
-    comment_info = {}
-    bulb_data = {}
-    
+    data = []
+
     Bulb.all.each do |bulbs|
+      bulb_id = bulbs.id
       bulb_owner = bulbs.user.name
+      bulb_created = bulbs.created_at
       bright_bulb = bulbs.bright
       dim_bulb = bulbs.dim
       blocked = bulbs.blocked
       likes = bulbs.likes
       panic_score = bulbs.panic_score
-      bulb_data[bulbs.id] = { "bulb writer" => bulb_owner, "bright" => bright_bulb, "dim" => dim_bulb, "blocked" => blocked, "likes" => likes, "panic_score" => panic_score, "comments" => comment_info[bulbs.id] = bulbs.comments}
+      comments = bulbs.user.comments
+
+      data << { "bulb_id" => bulb_id, "bulb created" => bulb_created, "bulb writer" => bulb_owner, "bright" => bright_bulb, "dim" => dim_bulb, "blocked" => blocked, "likes" => likes, "panic_score" => panic_score, "comment data" => comments}
     end
 
-    render json: bulb_data
+    render json: data
   end
 
   def create

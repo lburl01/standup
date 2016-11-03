ce343719df4f9d7f2152826dc52a08886e66be9d
Lori Burleson <lori.burleson@gmail.com>
2016-11-03 15:31:31 -0400

    printing comment data for all bulbs and all bulbs with meta data as json

--- app/controllers/bulbs_controller.rb
+++ app/controllers/bulbs_controller.rb
@@ -1,9 +1,14 @@
 class BulbsController < ApplicationController
 
   def index
-    # bulbs = Bulb.all
-    @bulbs_usersnames = User.select("name, bright, dim, blocked, likes, bulbs.id as id").joins("INNER JOIN bulbs ON bulbs.user_id = users.id").where("is_deleted = false")
-    render json: @bulbs_usersnames
+    bulb_data = Bulb.select("bulbs.id as bulb_id, user_id, name as username, bright, dim, blocked, likes, panic_score").joins("INNER JOIN users ON users.id = bulbs.user_id").where("is_deleted = false").all
+    comment_data = Comment.select("comments.id as comment_id, comments.user_id, comment, comments.created_at as created_at").joins("INNER JOIN bulbs ON comments.bulb_id = bulbs.id").where("is_deleted = false").all
+    # all_data = {}
+    all_data = {
+      "bulb_data" => bulb_data,
+      "comment_data" => comment_data
+    }
+    render json: all_data
   end
 
   def create

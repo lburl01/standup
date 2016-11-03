8d601df4366911b3b2a64a5e0c43f66cff5b2a13
Lori Burleson <lori.burleson@gmail.com>
2016-11-02 20:54:55 -0400

    Will not add a user that whose email already exists in the database

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,12 @@
 class SessionsController < ApplicationController
   def create
     @auth = env["omniauth.auth"]
-    @user = User.from_omniauth(@auth)
-    session[:user_id] = @user.id
+    if User.exists?(email: @auth.info.email)
+      true
+    else
+      @user = User.from_omniauth(@auth)
+      session[:user_id] = @user.id
+    end
     redirect_to bulbs_index_path
   end
 end

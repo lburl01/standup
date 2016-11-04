76753aa2d57c24dd790ced838958eddcbdd8ef41
Samantha Davis <samantha.shea.davis@gmail.com>
2016-11-03 20:52:32 -0400

    added styling to usersHome and functions to take form data and post to database

--- app/assets/javascripts/controllers/bulbs_index_controller.js
+++ app/assets/javascripts/controllers/bulbs_index_controller.js
@@ -5,12 +5,11 @@ this.getBulbs = function() {
   $.get('index', function(response) {
     console.log(response);
     console.log(response[0].comment_data[0].comment);
-    console.log('in');
   });
 };
   // $q.when(bulbs.get()).then(function(response) {
   //   console.log(response);
   // });
   this.getBulbs();
-  
+
 });
--- app/assets/javascripts/controllers/users_home_controller.js
+++ app/assets/javascripts/controllers/users_home_controller.js
@@ -1,4 +1,28 @@
 angular.module('standupApp').controller('UsersHomeController', function() {
-  this.message = 'in UsersHomeController';
-  this.score = [1,2,3,4,5,6,7,8,9,10];
+    this.message = 'in UsersHomeController';
+
+    this.score = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    this.bulb={
+      bright: '',
+      dim: '',
+      blocked: '',
+      panic_score: '',
+    };
+
+    this.getPanicScore = function(score) {
+        this.bulb.panic_score = score;
+    };
+
+    this.createBulb = function(brightBulb, dimBulb, blocker) {
+        console.log(this.bulb);
+        $.ajax({
+          type: 'POST',
+          url: 'users/home',
+          data: this.bulb,
+          success: function() {
+            console.log('Ajax post worked!');
+          }
+        });
+    };
 });

5b5be7fb9a333f2dec8ce70e54ca526295521a14
Lori Burleson <lori.burleson@gmail.com>
2016-11-04 09:24:29 -0400

    bulb model tests all passing

--- app/models/bulb.rb
+++ app/models/bulb.rb
@@ -1,5 +1,8 @@
 class Bulb < ApplicationRecord
-  validates :bright, :dim, :blocked, presence: true
+  validates :bright,  presence: true
+  validates :dim, presence: true
+  validates :blocked, presence: true
+  validates :panic_score, format: { with: /\d/ }
 
   belongs_to :user
   has_many :comments

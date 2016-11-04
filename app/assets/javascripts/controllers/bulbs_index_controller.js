59bdcc09aea04d50ea08c2ca5911c53603d548d0
Samantha Davis <samantha.shea.davis@gmail.com>
2016-11-04 16:09:34 -0400

    posting bulbs on timeline

--- app/assets/javascripts/controllers/bulbs_index_controller.js
+++ app/assets/javascripts/controllers/bulbs_index_controller.js
@@ -1,14 +1,11 @@
 angular.module('standupApp').controller('BulbsIndexController', function($http, getBulbsService) {
 
 var self = this;
-this.test = '';
 
 var currentBulb = getBulbsService.get();
 currentBulb.then(function(response) {
   console.log(response);
-  this.test = response;
+  self.allBulbs = response;
 });
-
-console.log(self.test);
 
 });

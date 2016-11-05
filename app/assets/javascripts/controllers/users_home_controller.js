eaace5c817d5a2e4f25a7b2a3161b89bd8e5fdc4
Samantha Davis <samantha.shea.davis@gmail.com>
2016-11-05 18:10:57 -0400

    work on graph styles and functionality

--- app/assets/javascripts/controllers/users_home_controller.js
+++ app/assets/javascripts/controllers/users_home_controller.js
@@ -4,11 +4,11 @@ angular.module('standupApp').controller('UsersHomeController', ["getBulbsService
 
     this.score = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-    this.bulb={
-      bright: '',
-      dim: '',
-      blocked: '',
-      panic_score: '',
+    this.bulb = {
+        bright: '',
+        dim: '',
+        blocked: '',
+        panic_score: '',
     };
 
     this.getPanicScore = function(score) {
@@ -17,12 +17,12 @@ angular.module('standupApp').controller('UsersHomeController', ["getBulbsService
 
     this.createBulb = function(brightBulb, dimBulb, blocker) {
         $.ajax({
-          type: 'POST',
-          url: 'home',
-          data: this.bulb,
-          success: function() {
-            // do something?
-          }
+            type: 'POST',
+            url: 'home',
+            data: this.bulb,
+            success: function() {
+                // do something?
+            }
         });
     };
 
@@ -50,25 +50,61 @@ angular.module('standupApp').controller('UsersHomeController', ["getBulbsService
     var history = getBulbsService.getHist();
 
     self.getHistory = function() {
-      history.then(function(response) {
-        console.log(response);
-        self.bulbHist = response;
-        self.panicScore(self.bulbHist);
-      });
+        history.then(function(response) {
+            // console.log(response);
+            self.bulbHist = response;
+            self.panicScore(self.bulbHist);
+        });
     };
 
     self.panicScores = [];
 
     self.panicScore = function(response) {
-      response.forEach(function(item) {
-        if(item.panic_score === null) {
-          item.panic_score = 0;
-        }
-        self.score = {score : item.panic_score};
-        self.panicScores.push(self.score);
-      });
-      console.log(self.panicScores);
+        response.forEach(function(item) {
+            if (item.panic_score === null) {
+                item.panic_score = 0;
+            }
+            var timeString = item.created_at;
+
+            var shortenedTime = timeString.slice(5, 10);
+
+            console.log(shortenedTime);
+
+            self.score = {
+                score: item.panic_score,
+                time: shortenedTime
+            };
+
+            self.panicScores.push(self.score);
+        });
+
+        self.buildGraph(self.panicScores);
+        console.log(self.panicScores);
+
     };
+
+    self.buildGraph = function(scores) {
+        for (count = 0; count < scores.length; count++) {
+            var width = 0.7 / scores.length * 100 + '%';
+            var height = scores[count].score * 40 + 'px';
+            var left = 20;
+            var bar = $('<li>').attr('class', 'bar').css({
+                "height": height,
+                "width": width
+            }).text(scores[count].time).appendTo('.graphContainer').append('<p>'+ scores[count].score + '</p>');
+        }
+        // $('.bar').each(function(index) {
+        //   console.log(index);
+        // });
+
+    };
+
+
+
+
+
+
+
 
     self.getHistory();
 }]);

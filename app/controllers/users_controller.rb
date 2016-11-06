ad7e73716de8667edc4a7d6978892b137fcf7637
Lori Burleson <lori.burleson@gmail.com>
2016-11-06 18:04:43 -0500

    Added redirect when user is not logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,14 @@ class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def home
-    @users_bulbs = Bulb.select("bright, dim, blocked, likes, panic_score, created_at").where(user_id: current_user.id).where("is_deleted = false").all
-    respond_to do |format|
-      format.json { render json: @users_bulbs }
-      format.html { render :home }
-    end
+    if current_user.nil?
+      redirect_to pages_index_path
+    else
+      @users_bulbs = Bulb.select("bright, dim, blocked, likes, panic_score, created_at").where(user_id: current_user.id).where("is_deleted = false").all
+      respond_to do |format|
+        format.json { render json: @users_bulbs }
+        format.html { render :home }
+      end
   end
 
   def create
